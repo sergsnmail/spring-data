d54b42471062c48bae2a9c6f57e2ea9946a32335
Sergey Pozdeev <sergsnmail@gmail.com>
2021-07-11 15:55:41 +0300

    Homework-7

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,12 @@
+create table products (id bigserial primary key, title varchar(255), cost float );
+insert into products (title, cost) values
+('Яблоки',10.50),
+('Бананы',3.85),
+('Апельсины',4.20),
+('Мандарины',8.15),
+('Авокадо',17.42),
+('Груши',12.11),
+('Киви',6.38),
+('Персики',9.00),
+('Сливы',10.00),
+('Грейпфрут',25.15);
